d4b2e5fc9d259d8e2181557888de2720ec90a460
GuyDav <guy.david@softwire.com>
2024-07-03 10:01:55 +0100

    Final

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,64 +1,74 @@
 // This is our main function
-function fizzbuzz(r) {
-    let final = []
-    const args = process.argv
-    let range = args[2]
-    let rules = args.slice(3, args.length)
+const readline = require("readline")
 
-    if (args.length == 2){
-        range = r+1
-        rules = ['3','5','7','11','13','17']
-    }
+function fizzbuzz(r1) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question("Range for FizzBuzz ", function (r){
+        let range = r;
+
+        let final = []
+        const args = process.argv
+        //range = args[2]
+        let rules = args.slice(3, args.length)
+
+        if (args.length == 2){
+            rules = ['3','5','7','11','13','17']
+        }
 
 
-    for (let i = 1; i < range; i++){
-        let words = []
-        if (i%3 == 0 && rules.includes('3')){
-            words.push("Fizz")
-        }
-        if (i%5 == 0 && rules.includes('5')){
-            words.push("Buzz")
-        }
-        if (i%7 == 0 && 7 && rules.includes('7')){
-            words.push("Bang")
-        }
-        if (i%11 == 0 && rules.includes('11')){
-            words = ["Bong"]
-        }
-        if (i%13 == 0 && rules.includes('13')){
+        for (let i = 1; i < range; i++){
+            let words = []
+            if (i%3 == 0 && rules.includes('3')){
+                words.push("Fizz")
+            }
+            if (i%5 == 0 && rules.includes('5')){
+                words.push("Buzz")
+            }
+            if (i%7 == 0 && 7 && rules.includes('7')){
+                words.push("Bang")
+            }
+            if (i%11 == 0 && rules.includes('11')){
+                words = ["Bong"]
+            }
+            if (i%13 == 0 && rules.includes('13')){
+                if (words.length == 0){
+                    words = ["Fezz"]
+                } else{
+                    let temp = 0 
+                    while (temp < words.length){
+                        if (words[temp][0] == "B"){
+                            words = words.slice(0, temp) + "Fezz" + words.slice(temp, words.length)
+                            break
+                        } 
+                        temp++
+                    }       
+                }
+            }
+            if (i%17 == 0 && rules.includes('17')){
+                words.reverse()
+            }
+
+
+            //Outputing words + numbers
             if (words.length == 0){
-                words = ["Fezz"]
-            } else{
-                let temp = 0 
-                while (temp < words.length){
-                    if (words[temp][0] == "B"){
-                        words = words.slice(0, temp) + "Fezz" + words.slice(temp, words.length)
-                        break
-                    } 
-                    temp++
-                }       
+                //console.log(i)
+                final.push(i)
+            } else {
+                let word = ""
+                for (let j = 0; j < words.length; j++){
+                    word += words[j]
+                }
+                //console.log(word)
+                final.push(word)
             }
         }
-        if (i%17 == 0 && rules.includes('17')){
-            words.reverse()
-        }
-
-
-        //Outputing words + numbers
-        if (words.length == 0){
-            //console.log(i)
-            final.push(i)
-        } else {
-            let word = ""
-            for (let j = 0; j < words.length; j++){
-                word += words[j]
-            }
-            //console.log(word)
-            final.push(word)
-        }
-    }
-
-    return final
+        console.log(final)
+        return final
+    })
+    return 0
 }
 
 // Now, we run the main function:
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,13 +1,16 @@
 const fizzbuzz = require('./fizzbuzz')
 test('FizzBuzz for multiples of 3', () => { 
-    expect(fizzbuzz(3)).toContain('Fizz'); 
+    expect(fizzbuzz(3)[-1]) == 'Fizz'; 
 })
+
 test('FizzBuzz for multiples of 5', () => { 
-    expect(fizzbuzz(5)).toContain('Buzz'); 
+    expect(fizzbuzz(5)[-1]) == 'Buzz'; 
 })
+
 test('Fizzbuzz for multiples of 3 & 5', () => { 
     expect(fizzbuzz(15)[-1]) == 'FizzBuzz'; 
 })
+
 test('Fizzbuzz for multiples of 11', () => { 
     expect(fizzbuzz(33)[-1]) == 'Bong'; 
 })
@@ -15,9 +18,11 @@ test('Fizzbuzz for multiples of 11', () => {
 test('Fizzbuzz for multiples of 13', () => { 
     expect(fizzbuzz(65)[-1]) == 'FezzBuzz'; 
 })
+
 test('Fizzbuzz for multiples of 13 and 11', () => { 
     expect(fizzbuzz(143)[-1]) == 'FezzBong'; 
 })
+
 test('Fizzbuzz for multiples of 17', () => { 
     expect(fizzbuzz(255)[-1]) == 'BuzzFizz'; 
 })
